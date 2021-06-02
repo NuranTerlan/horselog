ea157a16135731c19b08be0f4268ba6dd0e063e9
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-06-02 15:58:28 +0400

    generate n task (logger bot) at start point of program

---
+++ MultiTaskLogging/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiTaskLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var taskList = Generators.GenerateNLoggerBot(20);
+            Task.WaitAll(taskList.ToArray());
+        }
+    }
+}
