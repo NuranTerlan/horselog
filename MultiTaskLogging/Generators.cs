5ec39bf4b31753ad32302bcc8bb8e41e49f9926f
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-06-02 15:57:55 +0400

    Create Generators.cs

---
+++ MultiTaskLogging/Generators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MultiTaskLogging
+{
+    public static class Generators
+    {
+        public static IEnumerable<Task> GenerateNLoggerBot(int n)
+        {
+            var returnedTasks = new List<Task>();
+            for (var i = 0; i < n; i++)
+            {
+                returnedTasks.Add(Task.Run(() => LoggerBot.LogAction(Task.CurrentId)));
+            }
+        
+            return returnedTasks;
+        }
+    }
+}
