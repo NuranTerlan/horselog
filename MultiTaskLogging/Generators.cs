9c155fe9fda3fe85f41d6b66bb24eb649a355e19
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-06-03 18:12:28 +0400

    add writer task individually to our custom task pool (read thread pools)

--- MultiTaskLogging/Generators.cs
+++ MultiTaskLogging/Generators.cs
@@ -8,11 +8,15 @@ namespace MultiTaskLogging
         public static IEnumerable<Task> GenerateNLoggerBot(int n)
         {
             var returnedTasks = new List<Task>();
+
+            var loggerBot = new LoggerBot();
+            
             for (var i = 0; i < n; i++)
             {
-                returnedTasks.Add(Task.Run(async () => await LoggerBot.LogAction(Task.CurrentId)));
+                returnedTasks.Add(Task.Run(async () => await loggerBot.LogAction(Task.CurrentId)));
             }
-        
+            returnedTasks.Add(Task.Run(async () => await loggerBot.WriteToFile(Task.CurrentId)));
+
             return returnedTasks;
         }
     }
